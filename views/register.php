de96d163d41763276e30c5f696a7d5205214bc81
Jan <jan.madon@gmail.com>
2024-03-08 21:22:30 +0100

    added validation and showing errors on the form

--- views/register.php
+++ views/register.php
@@ -1,21 +1,34 @@
-<h1>Contact us</h1>
+<h1>Create an account</h1>
 
-<form action="/register" method="post">
-    <div class="mb-3">
-        <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="subject">
-    </div>
-    <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email">
-    </div>
-    <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password" name="password">
-    </div>
-    <div class="mb-3">
-        <label for="confirmPassword" class="form-label">Confirm Password</label>
-        <input type="password" class="form-control" id="confirmPassword" name="confirmPassword">
+<?php $form = \app\core\form\Form::begin('', 'post') ?>
+    <?php echo $form->field($model, 'firstname') ?>
+    <?php echo $form->field($model, 'lastname') ?>
+    <?php echo $form->field($model, 'email') ?>
+    <?php echo $form->field($model, 'password')->passwordField() ?>
+    <?php echo $form->field($model, 'confirmPassword')->passwordField() ?>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+<?php \app\core\form\Form::end() ?>
+
+
+<form action="" method="post">
+    <div class="row">
+        <div class="col">
+            <div class="form-group">
+                <label class="form-label">Firstname</label>
+                <input type="text" name="firstnam" value="<?php echo $model->firstname ?>"
+                       class="form-control<?php echo $model->hasError('firstname') ? 'is-invalid' : '' ?>">
+                <div class="invalid-feedback">
+                    <?php echo $model->getfirstError('firstname') ?>
+                </div>
+
+            </div>
+            <div class="col">
+                <label class="form-label">LastName</label>
+                <input type="text" name="lastname">
+            </div>
+        </div>
+
     </div>
 
     <button type="submit" class="btn btn-primary">Submit</button>
