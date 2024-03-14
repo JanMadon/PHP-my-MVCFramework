0f065011249d869ca18025d8dc605ddbb1afb6b8
Jan <jan.madon@gmail.com>
2024-03-14 19:34:03 +0100

    added login page

--- views/login.php
+++ views/login.php
@@ -1,1 +1,13 @@
-<h1>You are logged</h1>
+<?php
+/** @var $model \app\models\User */
+
+?>
+
+<h1>login</h1>
+
+<?php $form = \app\core\form\Form::begin('', 'post') ?>
+<?php echo $form->field($model, 'email') ?>
+<?php echo $form->field($model, 'password')->passwordField() ?>
+
+<button type="submit" class="btn btn-primary">Submit</button>
+<?php \app\core\form\Form::end() ?>
